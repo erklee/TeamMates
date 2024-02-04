0f2031cf0deb7cea41d0118f2d77db9feca62d33
Eric Lee <erlee92@yahoo.com>
2024-02-04 11:28:34 -0500

    user index

--- frontend/src/components/UserIndexPage/UserItem/UserItem.jsx
+++ frontend/src/components/UserIndexPage/UserItem/UserItem.jsx
@@ -3,26 +3,37 @@ import { sendFriendRequestThunk, unfriendThunk } from "../../../store/friends";
 
 const UserItem = ({ user }) => {
     const dispatch = useDispatch();
-    const currentUserId = useSelector(state => state.session.user?._id);
-    const friends = useSelector(state => state.friends.friends); 
-    const isFriend = friends.some(friend => friend._id === user._id);
-    
-    const handleFriendButtonClick = () => {
-        if (isFriend) {
-          dispatch(unfriendThunk(user._id));
-        } else {
-          dispatch(sendFriendRequestThunk(user._id));
-        }
-      };
-    
-      return (
-        <li>
-          {user.fname} {user.lname} ({user.username}) - {user.email}
-          <button onClick={handleFriendButtonClick}>
-            {isFriend ? 'Remove Friend' : 'Add Friend'}
-          </button>
-        </li>
-      );
+  
+    const handleSendFriendRequest = () => {
+      dispatch(sendFriendRequestThunk(user._id));
     };
-    
-    export default UserItem;
+  
+    const handleAcceptFriendRequest = () => {
+      dispatch(acceptFriendRequestThunk(user._id));
+    };
+  
+    const handleUnfriend = () => {
+      dispatch(unfriendThunk(user._id));
+    };
+  
+    const renderActionButton = () => {
+      if (user.isFriend) {
+        return <button onClick={handleUnfriend}>Unfriend</button>;
+      } else if (user.friendRequestStatus === "received") {
+        return <button onClick={handleAcceptFriendRequest}>Accept</button>;
+      } else if (user.friendRequestStatus === "pending") {
+        // Optionally provide a way to cancel the friend request here
+        return <span>Pending</span>;
+      } else {
+        return <button onClick={handleSendFriendRequest}>Send Friend Request</button>;
+      }
+    };
+  
+    return (
+      <li>
+        {user.fname} {user.lname} - {renderActionButton()}
+      </li>
+    );
+};
+
+export default UserItem
