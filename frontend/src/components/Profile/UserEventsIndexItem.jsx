7a4f128f6bb2506c8161f042105b63d7bf484697
Shaun Jhingoor <Jhingoor1945@gmail.com>
2024-02-12 10:00:29 -0500

    errors

--- frontend/src/components/Profile/UserEventsIndexItem.jsx
+++ frontend/src/components/Profile/UserEventsIndexItem.jsx
@@ -63,12 +63,12 @@ export default function UserEventsIndexItem({event}) {
 
             <p><span>Description:</span> {`${event.description}`}</p>
             <p><span>Sport:</span> {`${event.category?.slice(0,1).toUpperCase()}${event.category?.slice(1)}`}</p>
-            <p><span>Difficulty:</span> {`${event.difficulty?.slice(0,1).toUpperCase()}${event.difficulty?.slice(1)}`}</p>
-            <p><span>Address:</span> {`${event.location?.address} ${event.location?.zipcode}`}</p>
+            <p><span>Difficulty:</span> {`${event?.difficulty?.slice(0,1).toUpperCase()}${event.difficulty?.slice(1)}`}</p>
+            <p><span>Address:</span> {`${event?.location?.address} ${event.location?.zipcode}`}</p>
             <p><span>Date:</span> {`${formatDate(event?.date)}`}</p>
             <p><span>Time:</span> {`${formattedTime}`}</p>
-            <p><span>Participants:</span> {`${event.attendees?.length}`}</p>
-            <a target="_blank" href={`https://maps.google.com/?q=${event.location.address}, ${event.location.zipcode}`} rel="noreferrer">Directions</a>
+            <p><span>Participants:</span> {`${event?.attendees?.length}`}</p>
+            <a target="_blank" href={`https://maps.google.com/?q=${event?.location?.address}, ${event.location.zipcode}`} rel="noreferrer">Directions</a>
           </div>
         </div>
       </div>
