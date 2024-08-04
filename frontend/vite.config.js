cacb7891bf83621746806b81135423c599209eb4
Eric Lee <erlee92@yahoo.com>
2024-08-04 16:26:41 -0400

    vite config production backend test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
@@ -13,14 +12,19 @@ export default defineConfig(({ mode }) => ({
   ],
   server: {
     proxy: {
-      "/api": "https://team-mates-backend.vercel.app/",
+      // Proxy local API calls to the backend during development
+      "/api": {
+        target: mode === "production" ? "https://team-mates-backend.vercel.app/" : "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
     },
     // Uncomment this if you want to open the browser automatically
     // open: true
   },
   build: {
     rollupOptions: {
-      input: 'index.html', // Ensure the input is set to index.html
+      input: 'index.html',
     },
   },
 }));
